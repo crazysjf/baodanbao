7eb0f3d650b06f43aab2fa8da2670ef4d7e8e366
sjf <crazysjf@gmail.com>
2017-05-20 00:40:49 +0800

    列表中插入复选框

--- multi_selection_dialog.py
+++ multi_selection_dialog.py
@@ -1,9 +1,14 @@
 # -*- coding: utf-8 -*-
 from multi_selection import Ui_Dialog
-from PyQt5.QtWidgets import QApplication, QDialog
+from PyQt5.Qt import *
+from PyQt5.QtWidgets import  QApplication, QDialog, QTableWidgetItem
 
 class MultiSelectionDialog(QDialog, Ui_Dialog):
     def __init__(self):
         super(MultiSelectionDialog, self).__init__()
         self.setupUi(self)
+        self.tableWidget.setRowCount(3)
+        cb = QTableWidgetItem()
+        cb.setCheckState(Qt.Checked)
+        self.tableWidget.setItem(0,1,cb)
 
